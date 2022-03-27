1dbda7a4db7bb36220ec65018fb0c58b8fc2c31c
JikGit <dnlbrll@gmail.com>
2022-03-27 19:58:36 +0200

    Prova per telefoni

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const btn = document.querySelector(".btn");
 const vittoriaDiv = document.querySelector(".vittoria");
 const sconfittaDiv = document.querySelector(".sconfitta");
 const sconfittaText = document.querySelector(".sconfitta>p");
+const errore = document.querySelector(".errore");
 const menuButton = document.getElementById("menuButton");
 const closeMenuButton = document.querySelector("#menuDisplay > div");
 const menuDisplay = document.getElementById("menuDisplay");
@@ -22,7 +23,7 @@ setDimensionContainer(container);
 
 btnLettere.addEventListener("click", () => {
     var radios = document.getElementsByName('lettere');
-    for (var radio of radios){
+    for (var radio of radios) {
         if (radio.checked) {
             removeElement();
             resetAllValue(radio.value, 5);
@@ -44,14 +45,14 @@ menuButton.addEventListener("click", () => {
 
 
 btn.addEventListener("click", () => {
-    for (var x = 0; x < chanceUsate; x++){
-        for (var i = 0; i < rowElement; i++){
+    for (var x = 0; x < chanceUsate; x++) {
+        for (var i = 0; i < rowElement; i++) {
             listLetter[x][i].innerHTML = "";
             listLetter[x][i].classList.remove("green")
             listLetter[x][i].classList.remove("yellow")
         }
     }
-    listRow = []; 
+    listRow = [];
     step = 0;
     chanceUsate = 0;
     vittoriaDiv.classList.remove("visibile")
@@ -60,29 +61,29 @@ btn.addEventListener("click", () => {
 })
 
 // document.addEventListener('keyup', function keyPressed(event, phone = false) {
-inputContainer.addEventListener("keyup", (event) => {
-    var name = event.key;        
+inputContainer.addEventListener("keydown", (event) => {
+    var name = event.key;
 
-    if (btn.classList.contains("visibile")){
+    if (btn.classList.contains("visibile")) {
         return;
     }
     //premo enter
-    if (name == "Enter"){
-        if (listRow.length != rowElement || checkparola(listRow.join(''), rowElement) == -1){    
-            return;
-        }else{
+    if (name == "Enter") {
+        if (listRow.length != rowElement || checkparola(listRow.join(''), rowElement) == -1) {
+            errore.classList.add("visibile");
+        } else {
             //prima controllo per i vardi, cosi' se ci sono due lettere uguali una verde e l'altra gialla
-            for (var i = 0; i < rowElement; i++){
+            for (var i = 0; i < rowElement; i++) {
                 listLetter[chanceUsate][i].classList.remove("active");
                 //se la lettera c'e
-                if (parola[i] == listRow[i]){
+                if (parola[i] == listRow[i]) {
                     parola = parola.substring(0, i) + " " + parola.substring(i + 1);
                     listLetter[chanceUsate][i].classList.add("green")
                 }
             }
 
-            for (var i = 0; i < rowElement; i++){
-                if (parola.includes(listRow[i])){
+            for (var i = 0; i < rowElement; i++) {
+                if (parola.includes(listRow[i])) {
                     listLetter[chanceUsate][i].classList.add("yellow")
                 }
             }
@@ -90,23 +91,23 @@ inputContainer.addEventListener("keyup", (event) => {
             toggleSpin(listLetter, chanceUsate);
             chanceUsate++;
 
-            if (parola == listRow.join('')){
+            if (parola == listRow.join('')) {
                 vittoriaDiv.classList.add("visibile")
                 btn.classList.add("visibile")
             }
-            else if (chanceUsate == chance){
+            else if (chanceUsate == chance) {
                 sconfittaDiv.classList.add("visibile")
                 sconfittaText.innerHTML += parola;
                 btn.classList.add("visibile")
             }
-            listRow = []; 
+            listRow = [];
             step = 0;
         }
         return;
     }
     //se preme indietro va una lettera indietro
-    else if (name == "Backspace"){
-        if (step > 0){
+    else if (name == "Backspace") {
+        if (step > 0) {
             step--;
             listLetter[chanceUsate][step].innerHTML = "";
             listRow.pop();
@@ -114,18 +115,18 @@ inputContainer.addEventListener("keyup", (event) => {
         return;
     }
 
-    else if (isLetter(name) && step != rowElement){
+    else if (isLetter(name) && step != rowElement) {
         listLetter[chanceUsate][step].innerHTML = name;
         listRow.push(name);
         step++;
     }
-    else{
+    else {
         return
     }
     // listLetter[chanceUsate][step-1].classList.add("active");
 });
 
-function resetAllValue(lettere, nChance){
+function resetAllValue(lettere, nChance) {
     rowElement = lettere;
     chance = nChance;
     chanceUsate = 0;
@@ -136,39 +137,39 @@ function resetAllValue(lettere, nChance){
 }
 
 
-function setDimensionContainer(container){
-    container.style.setProperty("--width", rowElement*70+rowElement*5) 
-    container.style.setProperty("--height", chance*70+chance*5) 
+function setDimensionContainer(container) {
+    container.style.setProperty("--width", rowElement * 70 + rowElement * 5)
+    container.style.setProperty("--height", chance * 70 + chance * 5)
 }
 
-function removeElement(){
+function removeElement() {
     var allElement = document.querySelectorAll(".letter");
-    for (elm of allElement){
+    for (elm of allElement) {
         container.removeChild(elm);
     }
 }
 
-function createElement(listLetter){
-	for(var i=0; i<chance; i++) {
-	    listLetter.push([]);
-	}
+function createElement(listLetter) {
+    for (var i = 0; i < chance; i++) {
+        listLetter.push([]);
+    }
 
-    for (var i = 0; i < rowElement*chance; i++){
+    for (var i = 0; i < rowElement * chance; i++) {
         const elm = document.createElement("div");
         elm.classList.add("letter");
-        listLetter[Math.floor(i/rowElement)].push(elm);
+        listLetter[Math.floor(i / rowElement)].push(elm);
         container.appendChild(elm);
     }
 }
 
 function isLetter(str) {
-  return str.length === 1 && str.match(/[a-z]/i);
+    return str.length === 1 && str.match(/[a-z]/i);
 }
 
-function toggleSpin(listLetter, chanceUsate){
-    for (var i = 0; i < rowElement; i++){
+function toggleSpin(listLetter, chanceUsate) {
+    for (var i = 0; i < rowElement; i++) {
         listLetter[chanceUsate][i].classList.toggle("spin");
-        listLetter[chanceUsate][i].style.setProperty("--delaySpin", i/7);
+        listLetter[chanceUsate][i].style.setProperty("--delaySpin", i / 7);
     }
 }
 
